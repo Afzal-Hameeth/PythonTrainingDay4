eee039e3ef7a2b10839c6633abc4f0956968f265
Afzal-Hameeth <2000163830@hexaware.com>
2025-09-13 08:28:39 +0530

    Update main.py

--- Employeemanagement/main.py
+++ Employeemanagement/main.py
@@ -2,13 +2,14 @@ from employee import EmployeeDB
 
 db=EmployeeDB()
 
-db.add_employee(3,"tausifa","fs")
-db.add_employee(4,"balaji","bfs")
+db.add_employee(1,"Afzal","PGET")
+db.add_employee(2,"Pravin","PGET")
 
-db.check_in(3)
-db.check_in(4)
+db.check_in(1)
+db.check_in(2)
 
-db.check_out(3)
-db.check_out(4)
+db.check_out(1)
+db.check_out(2)
+
 
 db.show_attendance()
