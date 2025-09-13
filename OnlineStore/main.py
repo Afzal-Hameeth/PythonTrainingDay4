d762c76e9cd83345c4fe5432b3135562505b1989
Afzal-Hameeth <2000163830@hexaware.com>
2025-09-13 08:30:57 +0530

    Update main.py

--- OnlineStore/main.py
+++ OnlineStore/main.py
@@ -3,11 +3,12 @@ from product import ProductCatalogDB
 db=ProductCatalogDB()
 
 db.add_category("fashion")
-db.add_product("Shirt",1,600,10)
-db.add_product("Jeans",1,1200,2)
+db.add_product("Shirt",1,400,5)
+db.add_product("Jeans",1,800,3)
 #
-db.update_product(1,500,11)
+db.update_product(1,600,11)
 db.delete_product(2)
-db.search_products(1000)
+db.search_products(800)
 db.low_stock_report(5)
+
 db.show_products()
